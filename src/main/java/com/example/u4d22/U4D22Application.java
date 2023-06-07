3b8128e05aa42986d00a0947030cb8f65935a976
Nello Casolla <nellocasolla446@gmail.com>
2023-06-07 14:16:36 +0200

    assets

---
+++ src/main/java/com/example/u4d22/U4D22Application.java
@@ -0,0 +1,13 @@
+package com.example.u4d22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4D22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4D22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/u4d22/U4D22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.u4d22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4D22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
